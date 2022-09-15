7da2ddf5176415a98ca48d148938dd63528bf3c8
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-09-16 05:58:54 +0700

    add react-redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import './styles/icons/icons.css';
 import App from './App';
+import store from './app/store';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
