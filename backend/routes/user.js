b9fed260c11a80d97d57f460d3383577fc871c66
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-09-15 21:12:20 +0700

    add activate account

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { register } from '../controllers/user';
+import { activateAccount, register } from '../controllers/user';
 
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/activate', activateAccount);
 
 export default router;
