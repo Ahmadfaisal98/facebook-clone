076e6fb0dcfe736f063f58bf20fa419bad810db1
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-09-16 19:14:44 +0700

    update extension to scss and jsx

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import './index.css';
-import './styles/icons/icons.css';
+import './index.scss';
+import './styles/icons/icons.scss';
 import App from './App';
 import store from './app/store';
 
