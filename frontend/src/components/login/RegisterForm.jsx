bacc09ed4d0009942090ee6b4bf615b34fe073b2
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-09-17 07:33:01 +0700

    update value options register form

--- frontend/src/components/login/RegisterForm.jsx
+++ frontend/src/components/login/RegisterForm.jsx
@@ -3,30 +3,58 @@ import { useState } from 'react';
 
 import RegisterInput from '../inputs/registerInput';
 
-const userInfos = {
-  first_name: '',
-  last_name: '',
-  email: '',
-  password: '',
-  bYear: '',
-  bMonth: '',
-  bDay: '',
-  gender: '',
-};
-
 export default function RegisterForm() {
+  const userInfos = {
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    bYear: new Date().getFullYear(),
+    bMonth: new Date().getMonth() + 1,
+    bDay: new Date().getDate(),
+    gender: '',
+  };
   const [user, setUser] = useState(userInfos);
+  const {
+    first_name,
+    last_name,
+    email,
+    password,
+    bYear,
+    bMonth,
+    bDay,
+    gender,
+  } = user;
+  const yearTemp = new Date().getFullYear();
   const handleRegisterChange = (e) => {
     const { name, value } = e.target;
     setUser({ ...user, [name]: value });
   };
+  const years = Array.from(new Array(108), (val, index) => ({
+    year: yearTemp - index,
+    key: index,
+  }));
+  const months = Array.from(new Array(12), (val, index) => ({
+    month: 1 + index,
+    key: index,
+  }));
+  const getDays = () => new Date(bYear, bMonth, 0).getDate();
+  const days = Array.from(new Array(getDays()), (val, index) => ({
+    day: 1 + index,
+    key: index,
+  }));
+  const toMonthName = (month) => {
+    const date = new Date(bYear, month, 0);
+    return date.toLocaleString('default', { month: 'short' });
+  };
+
   return (
     <div className='blur'>
       <div className='register'>
         <div className='register_header'>
           <i className='exit_icon'></i>
           <span>Sign Up</span>
-          <span>it's quick and easy</span>
+          <span>It's quick and easy</span>
         </div>
         <Formik>
           {(formik) => (
@@ -66,14 +94,38 @@ export default function RegisterForm() {
                   Date of birth <i className='info_icon'></i>
                 </div>
                 <div className='register_grid'>
-                  <select name='bDay'>
-                    <option>15</option>
+                  <select
+                    name='bDay'
+                    value={bDay}
+                    onChange={handleRegisterChange}
+                  >
+                    {days.map(({ day, key }) => (
+                      <option value={day} key={key}>
+                        {day}
+                      </option>
+                    ))}
                   </select>
-                  <select name='bMonth'>
-                    <option>15</option>
+                  <select
+                    name='bMonth'
+                    value={bMonth}
+                    onChange={handleRegisterChange}
+                  >
+                    {months.map(({ month, key }) => (
+                      <option value={month} key={key}>
+                        {toMonthName(month)}
+                      </option>
+                    ))}
                   </select>
-                  <select name='bYear'>
-                    <option>15</option>
+                  <select
+                    name='bYear'
+                    value={bYear}
+                    onChange={handleRegisterChange}
+                  >
+                    {years.map(({ year, key }) => (
+                      <option value={year} key={key}>
+                        {year}
+                      </option>
+                    ))}
                   </select>
                 </div>
               </div>
