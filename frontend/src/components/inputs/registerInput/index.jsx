d450eabd7ff67ac4035606b0c929277992e52023
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-09-17 10:30:13 +0700

    update responsive on register form

--- frontend/src/components/inputs/registerInput/index.jsx
+++ frontend/src/components/inputs/registerInput/index.jsx
@@ -5,58 +5,58 @@ import './style.scss';
 
 export default function RegisterInput({ placeholder, bottom, ...props }) {
   const [field, meta] = useField(props);
-  const desktopView = useMediaQuery({
-    query: '(min-width: 850px)',
+
+  const small = useMediaQuery({
+    query: '(min-width: 539px)',
   });
+  const large = useMediaQuery({
+    query: '(min-width: 1170px)',
+  });
+  const largeFirstName = large && field.name === 'first_name';
+  const largeLastName = large && field.name === 'last_name';
   return (
-    <div className='input'>
-      {meta.touched && meta.error && !bottom && (
-        <div
-          className={
-            desktopView ? 'input_error input_error_desktop' : 'input_error'
-          }
-          style={{ transform: 'translateY(3px)' }}
-        >
-          {meta.touched && meta.error && <ErrorMessage name={field.name} />}
-          {meta.touched && meta.error && (
-            <div
-              className={desktopView ? 'error_arrow_left' : 'error_arrow_top'}
-            ></div>
-          )}
-        </div>
-      )}
+    <div className='input register_input'>
       <input
         className={meta.touched && meta.error ? 'input_error_border' : ''}
+        style={{
+          width: `${
+            small && (field.name === 'first_name' || field.name === 'last_name')
+              ? '100%'
+              : small && (field.name === 'email' || field.name === 'password')
+              ? '370px'
+              : '300px'
+          }`,
+        }}
         type={field.type}
         name={field.name}
         placeholder={placeholder}
         {...field}
         {...props}
       />
-      {meta.touched && meta.error && bottom && (
+      {meta.touched && meta.error && (
         <div
-          className={
-            desktopView ? 'input_error input_error_desktop' : 'input_error'
-          }
-          style={{ transform: 'translateY(2px)' }}
+          className={large ? 'input_error input_error_desktop' : 'input_error'}
+          style={{
+            transform: 'translateY(2px)',
+            left: `${largeFirstName ? '-107%' : largeLastName ? '107%' : ''}`,
+          }}
         >
           {meta.touched && meta.error && <ErrorMessage name={field.name} />}
           {meta.touched && meta.error && (
             <div
               className={
-                desktopView ? 'error_arrow_left' : 'error_arrow_bottom'
+                large && field.name !== 'last_name'
+                  ? 'error_arrow_left'
+                  : large && field.name === 'last_name'
+                  ? 'error_arrow_right'
+                  : !large && 'error_arrow_bottom'
               }
             ></div>
           )}
         </div>
       )}
 
-      {meta.touched && meta.error && (
-        <i
-          className='error_icon'
-          style={{ top: `${!bottom && !desktopView ? '63%' : '15px'}` }}
-        ></i>
-      )}
+      {meta.touched && meta.error && <i className='error_icon'></i>}
     </div>
   );
 }
