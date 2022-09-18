4cf8b8e540b029a2a1514c35a37e7b06f216da72
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-09-18 22:12:22 +0700

    add redux persist

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
 import userSlice from '../features/userSlice';
 import { serverApi } from '../services/serverApi';
+import storage from 'redux-persist/lib/storage';
 
-export default configureStore({
-  reducer: { userSlice, [serverApi.reducerPath]: serverApi.reducer },
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const reducers = combineReducers({
+  user: userSlice,
+  [serverApi.reducerPath]: serverApi.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(serverApi.middleware),
+    getDefaultMiddleware({
+      // Redux persist
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(serverApi.middleware),
   devTools: process.env.NODE_ENV !== 'production',
 });
+
+export const persistor = persistStore(store);
