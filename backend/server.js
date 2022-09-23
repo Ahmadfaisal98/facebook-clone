51640c7f3b120f1395cf80b8b9a317bad67246c6
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-09-23 21:08:20 +0700

    add morgan

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes/index.js';
 import mongoose from 'mongoose';
+import morgan from 'morgan';
 
 const app = express();
 app.use(express.json());
+app.use(morgan('dev'));
 app.use(cors({ origin: ['http://localhost:3000', 'some other link'] }));
 
 app.use('/', routes);
