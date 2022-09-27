31b191a8fa0328458f5c86aa81e9305a166197de
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-09-27 22:56:30 +0700

    add error handler for conditional image input

--- routes/upload.js
+++ routes/upload.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { uploadImages } from '../controllers/upload';
 import imageUpload from '../middlewares/imageUpload';
+import { authUser } from '../middlewares/auth';
 
 const router = express.Router();
 
-router.post('/images', imageUpload, uploadImages);
+router.post('/images', authUser, imageUpload, uploadImages);
 
 export default router;
