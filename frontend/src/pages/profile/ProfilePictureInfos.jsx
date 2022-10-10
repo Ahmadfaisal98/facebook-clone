5862b89f84bd7bf2580918d47540dd1aa31cae82
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-10-10 09:32:59 +0700

    display friends pictures preview

--- frontend/src/pages/profile/ProfilePictureInfos.jsx
+++ frontend/src/pages/profile/ProfilePictureInfos.jsx
@@ -1,6 +1,9 @@
 import { useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 import ProfilePicture from '../../components/profilePicture';
 import Friendship from './Friendship';
+
 export default function ProfilePictureInfos({ profile, visitor, photos }) {
   const [show, setShow] = useState(false);
   const pRef = useRef(null);
@@ -34,8 +37,32 @@ export default function ProfilePictureInfos({ profile, visitor, photos }) {
                 `(${profile?.details?.otherName})`}
             </div>
           </div>
-          <div className='profile_friend_count'></div>
-          <div className='profile_friend_imgs'></div>
+          <div className='profile_friend_count'>
+            {profile?.friends && (
+              <div className='profile_card_count'>
+                {profile?.friends.length === 0
+                  ? ''
+                  : profile?.friends.length === 1
+                  ? '1 Friend'
+                  : `${profile?.friends.length} Friends`}
+              </div>
+            )}
+          </div>
+          <div className='profile_friend_imgs'>
+            {profile?.friends &&
+              profile.friends.slice(0, 6).map((friend, i) => (
+                <Link to={`/profile/${friend.username}`} key={i}>
+                  <img
+                    src={friend.picture}
+                    alt=''
+                    style={{
+                      transform: `translateX(${-i * 7}px)`,
+                      zIndex: `${i}`,
+                    }}
+                  />
+                </Link>
+              ))}
+          </div>
         </div>
       </div>
       {visitor ? (
