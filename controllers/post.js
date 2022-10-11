20468a89de7d7bd375498077a95e23c325dbf828
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-10-11 20:24:50 +0700

    update post api

--- controllers/post.js
+++ controllers/post.js
@@ -1,4 +1,6 @@
+import { generatePage } from '../helpers/generatePage';
 import Post from '../models/Post';
+import User from '../models/User';
 
 export const createPost = async (req, res) => {
   try {
@@ -11,14 +13,31 @@ export const createPost = async (req, res) => {
 
 export const getAllPosts = async (req, res) => {
   try {
-    const posts = await Post.find()
-      .populate('user', 'first_name last_name picture username gender')
-      .populate(
-        'comments.commentBy',
-        'first_name last_name picture username commentAt'
-      )
-      .sort({ createdAt: -1 });
-    res.json(posts);
+    const { page, pageSize } = req.query;
+    const { limit, skip } = generatePage(page, pageSize);
+
+    const followingTemp = await User.findById(req.user.id).select('following');
+    const following = followingTemp.following;
+    const promises = following.map((user) => {
+      return Post.find({ user: user })
+        .populate('user', 'first_name last_name picture username cover')
+        .populate('comments.commentBy', 'first_name last_name picture username')
+        .sort({ createdAt: -1 })
+        .limit(limit)
+        .skip(skip);
+    });
+    const followingPosts = await (await Promise.all(promises)).flat();
+    const userPosts = await Post.find({ user: req.user.id })
+      .populate('user', 'first_name last_name picture username cover')
+      .populate('comments.commentBy', 'first_name last_name picture username')
+      .sort({ createdAt: -1 })
+      .limit(limit)
+      .skip(skip);
+    followingPosts.push(...[...userPosts]);
+    followingPosts.sort((a, b) => {
+      return b.createdAt - a.createdAt;
+    });
+    res.json(followingPosts);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
