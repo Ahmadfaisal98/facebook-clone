451b669c6b0b083e891b2192c3baa99ad02e9735
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-10-14 18:04:08 +0700

    add loader in home page

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { HashLoader } from 'react-spinners';
 
 import Header from '../../components/header';
 import LeftHome from '../../components/home/left';
@@ -27,11 +28,17 @@ export default function Home({ setVisible }) {
         <Stories />
         {user.verified === false && <SendVerification user={user} />}
         <CreatePost user={user} setVisible={setVisible} />
-        <div className='posts'>
-          {posts?.map((post) => (
-            <Post key={post._id} post={post} user={user} />
-          ))}
-        </div>
+        {isLoading ? (
+          <div className='skeleton_loader'>
+            <HashLoader color='#1876f2' />
+          </div>
+        ) : (
+          <div className='posts'>
+            {posts?.map((post) => (
+              <Post key={post._id} post={post} user={user} />
+            ))}
+          </div>
+        )}
       </div>
       <RightHome user={user} />
     </div>
