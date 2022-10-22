c3cfc75b959493e881142d2d95682740838718ce
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-10-22 22:40:54 +0700

    remove cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import fileUpload from 'express-fileupload';
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(cors({ origin: ['http://localhost:3000', process.env.FRONTEND_URL] }));
+app.use(cors());
 app.use(
   fileUpload({
     useTempFiles: true,
