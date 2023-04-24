f2302a771bdf081a87ec2b724fcaa41792d84a78
Ahmad Faisal <achmad_fsl@yahoo.com>
2023-04-24 23:08:06 +0700

    updated code

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export const authUser = (req, res, next) => {
   try {
-    const token = req.header('Authorization')?.split(' ').at(-1);
+    const token = req.headers.authorization?.split(' ')[1];
     if (!token) {
       return res.status(400).json({ message: 'Invalid Authentication' });
     }
