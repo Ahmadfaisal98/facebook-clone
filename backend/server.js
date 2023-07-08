3f0055348ccb1b4dead8790e8836593cb923763b
Ahmad Faisal <achmad_fsl@yahoo.com>
2023-07-08 23:10:26 +0700

    updated app.listen

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import mongoose from 'mongoose';
 import morgan from 'morgan';
 import fileUpload from 'express-fileupload';
 
+const PORT = process.env.PORT || 3333;
+
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
@@ -22,11 +24,10 @@ mongoose
   .connect(process.env.DATABASE_URL)
   .then(() => {
     console.log('database connected successfully');
+    app.listen(PORT, () => {
+      console.log('Server is listening on port ' + PORT);
+    });
   })
   .catch((err) => console.log('error connecting to mongodb', err));
 
-const PORT = process.env.PORT || 3333;
 
-app.listen(PORT, () => {
-  console.log('Server is listening on port ' + PORT);
-});
